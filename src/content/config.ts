da5c5fe38a4c5a97c3146fd48334083355356d41
Iván Barcia <ivanbarcia@icloud.com>
2023-10-03 20:52:51 +0200

    create basic scaffold

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content'
+// import { CATEGORIES } from '@/data/categories'
+
+const status = defineCollection({
+	// Type-check frontmatter using a schema
+    type: 'content',
+	schema: () =>
+		z.object({
+			title: z.string().max(80),
+			tags: z.array(z.string()).optional(),
+			// Transform string to Date object
+			pubDate: z
+				.string()
+				.or(z.date())
+				.transform((val) => new Date(val)),
+			draft: z.boolean().default(false)
+		})
+})
+
+export const collections = { status }
--- src/content/status/20230912-ef73542d-4bf0-4f78-bd5f-6e95d63d4314/index.md
+++
@@ -1,7 +0,0 @@
----
-title: 'hello hola'
-slug: 'ef73542d-4bf0-4f78-bd5f-6e95d63d4314'
-datetime: '2022-10-12T16:30:00.000Z'
----
-
-Ola mundo
